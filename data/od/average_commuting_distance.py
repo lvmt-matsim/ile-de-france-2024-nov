e2045c50185a37e2d1d4ea206adcda3197c3ccee
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2024-12-07 12:30:35 +0100

    Update average_commuting_distance.py
    Complete with calculations

--- data/od/average_commuting_distance.py
+++ data/od/average_commuting_distance.py
@@ -8,45 +8,45 @@ Calculate the average commuting distance by commune
 def configure(context):
     context.stage("data.od.cleaned")
     context.stage("data.spatial.codes")
+    context.stage("data.spatial.centroid_distances")
 
-def fix_origins(df, commune_ids, purpose,category): 
+def fix_origins(df, commune_ids): 
     existing_ids = set(np.unique(df["origin_id"]))
     missing_ids = commune_ids - existing_ids
-    categories = set(np.unique(df[category]))
 
     rows = []
     for origin_id in missing_ids:
         for destination_id in commune_ids:
-            for category_name in categories :
-                rows.append((origin_id, destination_id, category_name, 1.0 if origin_id == destination_id else 0.0))
+            rows.append((origin_id, destination_id, 1.0 if origin_id == destination_id else 0.0))
 
-    print("Fixing %d origins for %s" % (len(missing_ids), purpose))
-
+    print("Fixing %d origins for commute" % (len(missing_ids)))
     return pd.concat([df, pd.DataFrame.from_records(
-        rows, columns = ["origin_id", "destination_id", category, "weight"]
+        rows, columns = ["origin_id", "destination_id", "weight"]
     )]).sort_values(["origin_id", "destination_id"])
-  
-# df_work["origin_id", "destination_id", "commute_mode","weight"]
+
 def execute(context):
-    # Municipalities list
+    # Load and clean data
+    df_work, df_education = context.stage("data.od.cleaned")
+    df_distances = context.stage("data.spatial.centroid_distances")
     df_codes = context.stage("data.spatial.codes")
     commune_ids = set(df_codes["commune_id"].unique())
 
-    # Load and clean data
-    df_work, df_education = context.stage("data.od.cleaned")
-    df_work = fix_origins(df_work, commune_ids, "work","commute_mode")
+    # Aggregate the flows
+    df_commute = pd.concat([df_work, df_education],axis=0)
+    df_commute = df_commute.groupby(["origin_id", "destination_id"])["weight"].sum().reset_index()
+    df_commute = fix_origins(df_commute, commune_ids)
 
-    # Aggregate work (we do not consider different modes at the moment)
-    df_pt = df_work[df_work.commute_mode == "pt"].reset_index(drop=True)
-    df_pt = df_pt[["origin_id", "weight"]].groupby(["origin_id"]).sum().reset_index()
+    # Add centroid and cumulated distances information
+    df_commute = pd.merge(left=df_commute, right=df_distances, how="left", on=["origin_id", "destination_id"])
+    df_commute["cumulated_commuting_distance"] = df_commute.apply(lambda row: row["centroid_distance"]*row["weight"], axis=1)
 
-    # Compute total and PT share
-    df_total = df_work[["origin_id", "weight"]].groupby("origin_id").sum().reset_index().rename({ "weight" : "total" }, axis = 1)
-    df_pt = pd.merge(left=df_pt, right=df_total, how="left", on = "origin_id")
-    df_pt["PT_share"] = df_pt.apply(lambda row: 100 * row["weight"] / row["total"], axis=1)
+    # Calculate mean commuting distance by commune
+    df_total = df_commute.groupby(["origin_id"])["weight"].sum().reset_index().rename({"weight":"total_flow"}, axis = 1)
+    df_commute = df_commute.groupby(["origin_id"])["cumulated_commuting_distance"].sum().reset_index()
+    df_commute = pd.merge(left=df_commute, right=df_total, how="left", on = "origin_id")
+    df_commute["mean_commuting_distance"] = df_commute.apply(lambda row: row["cumulated_commuting_distance"] / row["total_flow"], axis=1)
 
     # Clean output df
-    df_pt = df_pt[["origin_id","PT_share"]]
-    df_pt = df_pt.rename(columns={"origin_id":"commune"})
-    df_pt.to_csv("%s/%PT_share.csv" % (output_path, output_prefix), sep=";", index=None)
-    return df_pt
+    df_commute = df_pt[["origin_id","mean_commuting_distance"]].rename(columns={"origin_id":"commune"})
+    df_commute.to_csv("%s/%mean_commuting_distance.csv" % (output_path, output_prefix), sep=";", index=None)
+    return df_commute
